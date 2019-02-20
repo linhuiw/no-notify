95c936cc4ba0c02b3afd1aa02b3586e2220b7022
linhuiw <solesnip@gmail.com>
2019-02-20 13:30:59 +0800

    feat: add add_style method

--- src/js/background.js
+++ src/js/background.js
@@ -1,2 +1,22 @@
 import '../img/icon-128.png'
 import '../img/icon-34.png'
+
+chrome.tabs.onCreated.addListener(addStyle);
+chrome.tabs.onUpdated.addListener(function(tabId, info, tab) {
+  if (info.status == 'complete') {
+    addStyle(tab);
+  }
+});
+
+function addStyle(tab) {
+  var tabUrl = tab.url;
+  if (tabUrl && tabUrl.indexOf("douban.com") != -1) {
+    chrome.tabs.insertCSS(tab.id, {
+      code: `
+        #top-nav-doumail-link em {
+          display: none;
+        }
+      `
+    });
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -24,10 +24,6 @@ var options = {
     popup: path.join(__dirname, "src", "js", "popup.js"),
     options: path.join(__dirname, "src", "js", "options.js"),
     background: path.join(__dirname, "src", "js", "background.js"),
-    content: path.join(__dirname, "src", "js", "content.js"),
-  },
-  chromeExtensionBoilerplate: {
-    notHotReload: ["content"]
   },
   output: {
     path: path.join(__dirname, "build"),
